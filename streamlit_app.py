91da70c1cc00888a89a4384eba93e7173dc09545
sanjaykanth-l <131290361+sanjaykanth-l@users.noreply.github.com>
2023-04-19 22:47:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def main():
       largest_num = num1
     elif num2 > num1 and num2 > num3:
       largest_num = num2
-     else:
+    else:
       largest_num = num3
       
      st.write("The largest number is:", largest_num)
